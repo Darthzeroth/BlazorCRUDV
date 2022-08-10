6ad89384064665732294b84ced5a90fef61b1603
Oscar González <osc_egr89@hotmail.com>
2022-08-10 16:17:18 -0500

    Agregar archivos de proyecto.

---
+++ BlazorCRUDV/Data/Respuesta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDV.Data
+{
+    public class Respuesta<T>
+    {
+        public int Exito { get; set; } //Recibimos un entero para saber si se recibio algo por get
+        public string Mensaje { get; set; } //Mandamos mensaje de error o exito
+        public T Data { get; set; } //Se tendran datos
+
+    }
+}
